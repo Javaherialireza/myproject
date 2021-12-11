b600036d531f41cd9e70e1854c384c6873f204dd
alireza <mjavaheri.alireza@gmail.com>
2021-12-11 15:09:04 +0330

    add class moelAdmin userprofile and article

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import userprofile
+from .models import * 
+class userprofileAdmin(admin.ModelAdmin):
+    list_display = ["user" , "avatar" , "description"]
+admin.site.register(userprofile , userprofileAdmin)
 
-admin.site.register(userprofile)
 
+class articleAdmin(admin.ModelAdmin):
+    search_fields = ["title" , "content"]
+    list_display = ["title","created_at","category"]
+
+admin.site.register(article , articleAdmin)
 #admin.site.register(userprofile)
-
+#admin.site.register(article)
+#admin.site.register(category)
+class categoryAdmin(admin.ModelAdmin):
+    list_display = ["title" , "cover"]
+admin.site.register(category,categoryAdmin)
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from datetime import datetime
 import os
 from django.core.exceptions import ValidationError
+from ckeditor.fields import RichTextField
 
 def validate_file_extention(value):
     ext = os.path.splitext(value.name)[1]
@@ -11,19 +12,31 @@ def validate_file_extention(value):
         raise ValidationError("unsupprted file extension.")
 
 # Create your models here.
-class userprofile(modles.Model):
-    user = models.OneToOneField(user, on_delete=models.CASCADE)
+class userprofile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.FileField(upload_to="files/user/avatar",null= True,blank=True, validators=[validate_file_extention])
-    description = models.models.CharField( max_length=500,null=False,blank=False)
+    description = models.CharField( max_length=500,null=False,blank=False)
+    
+    def __str__(self):
+        return self.user.username
+
 
 class article(models.Model):
     title =models.CharField(max_length=140,null=False, blank=False)
     cover = models.FileField(upload_to="files/article_cover/",null=False,blank=False , validators=[validate_file_extention])
-    content = richtextfield()
+    content = RichTextField()
     created_at = models.DateTimeField(default=datetime.now , blank=False)
     category = models.ForeignKey('category', on_delete = models.CASCADE)
-    author = models.OneToOneField(userprofile, on_delete = models.CASCADE(collector, field, sub_objs, using))
+    author = models.OneToOneField(userprofile, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
 
 class category(models.Model):
     title = models.CharField(max_length=128 , null=False , blank=False)
     cover = models.FileField(upload_to="files/category_cover" , null=False , blank= False, validators=[validate_file_extention])
+
+    def __str__(self):
+        return self.title
+    
